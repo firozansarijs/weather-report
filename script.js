23518978eda81f2741d6c05ca44a6247ece09a59
firozansarjs <firoz.ansari.dev@gmail.com>
2026-07-05 13:26:23 +0530

    add some code in index and some styling, fix problems

--- script.js
+++ script.js
@@ -1,2 +1,10 @@
-const apikey = "https://api.weatherapi.com/v1/current.json?key=27bf9a2ad17b47d6b92174842262906&q=Delhi";
+const BASE_URL = "https://api.weatherapi.com/v1/current.json?key=27bf9a2ad17b47d6b92174842262906&q=@latest";
 
+const search = document.querySelector(".search-container input");
+
+search.addEventListener("keydown", (evt) => {
+   if(evt.key === "Enter"){
+    const city = search.value;
+    console.log(city);
+   }
+})
