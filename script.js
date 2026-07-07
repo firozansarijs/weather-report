b9ab0befd71800e258dc5831cea8fa613b0ea810
firozansarjs <firoz.ansari.dev@gmail.com>
2026-07-08 03:03:27 +0530

    add some js work, fix problems

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-const BASE_URL = `https://api.weatherapi.com/v1/current.json?key=27bf9a2ad17b47d6b92174842262906&q=search.value`;
-const card = document.querySelector(".weather-card");
+const cityName = document.querySelector(".city h1");
+const weatherIcon = document.querySelector(".weather-icon");
+const temp = document.querySelector(".temp");
+const sun = document.querySelector(".cond");
+const hum = document.querySelector(".detail-1");
+const win = document.querySelector(".detail-2");
     
 const search = document.querySelector(".search-container input");
 search.addEventListener("keydown", (evt) => {
    if(evt.key === "Enter"){
       const city = search.value;
-  const url = `https://api.weatherapi.com/v1/current.json?key=27bf9a2ad17b47d6b92174842262906&q=${city}`;
+      const url = `https://api.weatherapi.com/v1/current.json?key=27bf9a2ad17b47d6b92174842262906&q=${city}`;
 
-    fetch(url)
+       fetch(url)
       .then((response) => response.json())
       .then((data) => {
-         console.log(data.location.name);
-         console.log(data.current.temp_c);
-         console.log(data.current.condition.text);
-         console.log(data.current.humidity);
-         console.log(data.current.wind_kph);
+        weatherIcon.src = `https:${data.current.condition.icon}`;
+        cityName.textContent = data.location.name;
+        temp.textContent = data.current.temp_c;
+        sun.textContent = data.current.condition.text;
+        hum.textContent = data.current.humidity;
+        win.textContent = data.current.wind_kph;
       });
   }
 });
